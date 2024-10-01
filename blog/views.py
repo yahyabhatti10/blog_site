4091c449e3063b6767f109748108d5d7cf43f3fa
Muhammad Yahya <yahyabhatti019@gmail.com>
2024-10-01 17:17:33 +0500

    added urls and views structure

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def starting_page(request):
+    return render(request, "blog/index.html")
+
+
+def posts(request):
+    return HttpResponse("This is Posts Page")
+
+
+def post_detail(request):
+    return HttpResponse("This is Post Detail Page")
